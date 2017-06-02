f8ae5a1f7404cec983063ad5028740522243cbe0
Waasyl <lukasz482@wp.pl>
2017-06-02 17:54:51 +0200

    Initial commit

---
+++ src/com/lukasz/Application.java
@@ -0,0 +1,10 @@
+package com.lukasz;
+
+/**
+ * Created by RENT on 2017-06-02.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello wordl");
+    }
+}
