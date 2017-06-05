f8f95497c58a5c2433584a2620a7a091dd5dcf68
Waasyl <lukasz482@wp.pl>
2017-06-05 18:34:55 +0200

    Added builder pattern

---
+++ src/com/lukasz/builder/Application.java
@@ -0,0 +1,17 @@
+package com.lukasz.builder;
+
+/**
+ * Created by RENT on 2017-06-05.
+ */
+public class Application {
+    public static void main(String[] args) {
+        User user = User.builder()
+                .firstName("Lukasz")
+                .lastName("Was")
+                .age(21)
+                .phoneNumber("123445678")
+                .build();
+
+        System.out.println(user);
+    }
+}
